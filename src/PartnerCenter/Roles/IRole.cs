ae0ebe504b24aae02119151617c0ed6d6e6e9b86
Isaiah Williams <isaiah.williams@microsoft.com>
2019-08-15 23:57:41 -0500

    Adding the ability to get partner role members

---
+++ src/PartnerCenter/Roles/IRole.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Isaiah Williams. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Store.PartnerCenter.Roles
+{
+    /// <summary>
+    /// Represents the available role operations.
+    /// </summary>
+    public interface IRole : IPartnerComponent<string>
+    {
+        /// <summary>
+        /// Gets the partner role member collection operations.
+        /// </summary>
+        IRoleMemberCollection Members { get; }
+    }
+}
--- src/PartnerCenter/Roles/RoleCollectionOperations.cs
+++ src/PartnerCenter/Roles/RoleCollectionOperations.cs
@@ -4,14 +4,10 @@
 namespace Microsoft.Store.PartnerCenter.Roles
 {
     using System;
-    using System.Collections.Generic;
-    using System.Globalization;
     using System.Threading;
     using System.Threading.Tasks;
-    using Extensions;
     using Models;
     using Models.JsonConverters;
-    using Models.Query;
     using Models.Roles;
 
     /// <summary>
@@ -29,6 +25,23 @@ namespace Microsoft.Store.PartnerCenter.Roles
         }
 
         /// <summary>
+        /// Gets a partner role operations object.
+        /// </summary>
+        /// <param name="id">The partner role identifier.</param>
+        /// <returns>The partner role operations object.</returns>
+        public IRole this[string id] => ById(id);
+
+        /// <summary>
+        /// Gets a partner role operations object.
+        /// </summary>
+        /// <param name="id">The partner role identifier.</param>
+        /// <returns>The partner role operations object.</returns>
+        public IRole ById(string id)
+        {
+            return new RoleOperations(Partner, id);
+        }
+
+        /// <summary>
         /// Gets all roles that belong to the partner.
         /// </summary>
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
@@ -39,85 +52,6 @@ namespace Microsoft.Store.PartnerCenter.Roles
                 new Uri(
                     $"/{PartnerService.Instance.ApiVersion}/{PartnerService.Instance.Configuration.Apis.GetPartnerRoles.Path}",
                     UriKind.Relative),
-                new ResourceCollectionConverter<Role>(),
-                cancellationToken).ConfigureAwait(false);
-        }
-
-        /// <summary>
-        /// Queries for the list of partner roles.
-        /// - Count queries are not supported by this operation.
-        /// - You can set page size and sort option.
-        /// - You can navigate to other pages by specifying a seek query with the seek operation and the continuation
-        ///   token sent by the previous operation.
-        /// </summary>
-        /// <param name="query">A query to apply onto customer users. Check <see cref="QueryFactory" /> to see how
-        /// to build queries.</param>
-        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-        /// <returns>The roles that match the specified criteria.</returns>
-        public async Task<SeekBasedResourceCollection<Role>> QueryAsync(IQuery query, CancellationToken cancellationToken = default)
-        {
-            IDictionary<string, string> headers = null;
-            IDictionary<string, string> parameters;
-
-            query.AssertNotNull(nameof(query));
-
-            if (query.Type == QueryType.Seek)
-            {
-                query.Token.AssertNotNull(nameof(query.Token));
-
-                headers = new Dictionary<string, string>
-                {
-                    {
-                        PartnerService.Instance.Configuration.Apis.GetPartnerRoles.AdditionalHeaders.ContinuationToken,
-                        query.Token.ToString()
-                    }
-                };
-
-                parameters = new Dictionary<string, string>
-                {
-                    {
-                        PartnerService.Instance.Configuration.Apis.GetPartnerRoles.Parameters.SeekOperation,
-                        query.SeekOperation.ToString()
-                    }
-                };
-            }
-            else
-            {
-                parameters = new Dictionary<string, string>();
-
-                if (query.Type == QueryType.Indexed)
-                {
-                    query.PageSize = Math.Max(query.PageSize, 0);
-
-                    parameters.Add(
-                        PartnerService.Instance.Configuration.Apis.GetPartnerRoles.Parameters.Size,
-                        query.PageSize.ToString(CultureInfo.InvariantCulture));
-                }
-                else
-                {
-                    parameters.Add(
-                        PartnerService.Instance.Configuration.Apis.GetPartnerRoles.Parameters.Size,
-                        "0");
-                }
-
-                if (query.Sort != null)
-                {
-                    parameters.Add(
-                        PartnerService.Instance.Configuration.Apis.GetPartnerRoles.Parameters.SortDirection,
-                        query.Sort.SortDirection.ToString());
-
-                    parameters.Add(
-                       PartnerService.Instance.Configuration.Apis.GetPartnerRoles.Parameters.SortField,
-                       query.Sort.SortField.ToString(CultureInfo.InvariantCulture));
-                }
-            }
-
-            return await Partner.ServiceClient.GetAsync<SeekBasedResourceCollection<Role>>(
-                new Uri(
-                    $"/{PartnerService.Instance.ApiVersion}/{PartnerService.Instance.Configuration.Apis.GetPartnerRoles.Path}",
-                    UriKind.Relative),
-                headers,
-                parameters,
                 new ResourceCollectionConverter<Role>(),
                 cancellationToken).ConfigureAwait(false);
         }
