c2096eb1f8e046f148d49bdbe5552a7f7bf0d085
Isaiah Williams <idwilliams@outlook.com>
2019-12-02 20:15:42 -0600

    Updating the way daily rated usage line items are deserialized (#26)

---
+++ src/PartnerCenter/Models/JsonConverters/NestedResourceConverter.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Isaiah Williams. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Store.PartnerCenter.Models.JsonConverters
+{
+    using System;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Provides the ability to deserialize nested resources that are returned as string.
+    /// </summary>
+    public class NestedResourceConverter : JsonConverter
+    {
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="NestedResourceConverter" /> can write JSON.
+        /// </summary>
+        public override bool CanWrite => false;
+
+        /// <summary>
+        /// Determines whether this instance can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns><c>true</c> if this instance can convert the specified object type; otherwise, <c>false.</c></returns>
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The reader object to be read.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <param name="existingValue">The existing value of object being read.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        /// <returns>The object that represents the serialized JSON.</returns>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return JsonConvert.DeserializeObject(reader.Value.ToString(), objectType);
+        }
+
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The object to be used when writing.</param>
+        /// <param name="value">The object to be written to JSON.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
